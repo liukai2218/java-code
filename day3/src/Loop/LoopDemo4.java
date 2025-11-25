9c8d51f845fc772883f86d2f9db6d7f8215be328
liukai2218 <2089180634@qq.com>
2025-11-26 01:39:00 +0800

    循环嵌套练习

---
+++ day3/src/Loop/LoopDemo4.java
@@ -0,0 +1,12 @@
+package Loop;
+//循环嵌套练习：输出10行10列的6
+public class LoopDemo4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int J = 0; J < 10; J++) {
+                System.out.print("6");
+            }
+            System.out.println();
+        }
+    }
+}
